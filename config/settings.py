39617fa2539d811453b4474447253e704ca8a6c8
id8436 <id8436@naver.com>
2022-09-15 14:36:28 +0900

    db

--- config/settings.py
+++ config/settings.py
@@ -111,12 +111,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+from config import localsettings
+DATABASES = localsettings.databases(BASE_DIR)
 
 
 # Password validation
