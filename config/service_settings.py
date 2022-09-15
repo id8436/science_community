251e846404fb8cf542d13c8c32d099db6e9cfc6a
id8436 <id8436@naver.com>
2022-09-15 14:15:09 +0900

    dbsetting

--- config/service_settings.py
+++ config/service_settings.py
@@ -1,3 +1,14 @@
 from .settings import *
 
-DEBUG = False # DEBUG=True이면 개발모드, False면 운영모드로 인식한다.
+DEBUG = False  # DEBUG=True이면 개발모드, False면 운영모드로 인식한다.
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite',
+        'USER': 'user',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
