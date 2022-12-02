cbcdd0ae1e674c9978a9692e40a75c57b9cdfb60
id8436 <id8436@naver.com>
2022-12-02 16:13:49 +0900

    facebook_login

--- config/settings.py
+++ config/settings.py
@@ -57,7 +57,7 @@ INSTALLED_APPS = [
 'allauth.socialaccount.providers.kakao',
 'allauth.socialaccount.providers.google',
 'allauth.socialaccount.providers.facebook',
-'allauth.socialaccount.providers.instagram',
+'allauth.socialaccount.providers.apple',
 ]
 SITE_ID = 1
 # ACCOUNT_DEFAULT_HTTP_PROTOCOL='https' 페이스북 로그인 시도하면서 이것저것 해본 건데, 없어도 될듯?(채팅기능 http로 작동하는지 확인해보자.)
