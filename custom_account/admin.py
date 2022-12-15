18d4e1a80e335c6e06435c43ccae72bfea1b3a7c
id8436 <id8436@naver.com>
2022-12-15 14:06:37 +0900

    password_reset

--- custom_account/admin.py
+++ custom_account/admin.py
@@ -3,6 +3,6 @@ from .models import User, Notification  # 직접 등록한 모델
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username',)
+    list_display = ('username', 'nickname', 'email')
     exclude = ('password',)  # 사용자 상세 정보에서 비밀번호 필드를 노출하지 않음
 admin.site.register(Notification)
