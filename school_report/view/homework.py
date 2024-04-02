d05bf7d8687436efc18107aa69d17831409d4c82
id8436 <id8436@naver.com>
2024-04-02 10:14:24 +0900

    과제 복사 수정.

--- school_report/view/homework.py
+++ school_report/view/homework.py
@@ -457,7 +457,7 @@ def distribution(request, homework_id):  # [profile로 바꾸자.]
     except:
         submit_profile_ids = []
     available_profile_ids = homework_box.get_profiles_id()
-    teachers_queryset = models.Profile.objects.filter(id__in=homework.author_profile.id)
+    teachers_queryset = models.Profile.objects.filter(id=homework.author_profile.id)
     available_profile_ids.union(teachers_queryset)  # 교사 프로필의 id도 추가.
     filtered_available_profile_ids = [profile_id for profile_id in available_profile_ids if profile_id not in submit_profile_ids]
     filtered_profiles = models.Profile.objects.filter(id__in=filtered_available_profile_ids)
