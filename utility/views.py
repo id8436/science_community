5d8b3e9d88aa6316f07181af7faedacfeec0f3c1
id8436 <id8436@naver.com>
2024-04-02 09:53:07 +0900

    과제 복사 수정.

--- utility/views.py
+++ utility/views.py
@@ -39,13 +39,16 @@ def do_DB(request):
         user = submit.to_user
         base_homework = submit.base_homework
         box = base_homework.homework_box
-        try:  # 프로필이 없기도..
-            school = box.get_school_model()
-            profile = models.Profile.objects.filter(admin=user, school=school)
-            submit.to_profile = profile
-            submit.save()
-        except:
-            pass
+        profile = models.Profile.objects.filter(admin=user).first()
+        # try:  # 프로필이 없기도..
+        #     school = box.get_school_model()
+        #     profile = models.Profile.objects.filter(admin=user, school=school)
+        #     submit.to_profile = profile
+        #     submit.save()
+        # except:
+        #     pass
+        submit.to_profile = profile
+        submit.save()
     return render(request, 'utility/main.html', {})
 
 def do_DB2(request):
