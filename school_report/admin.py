98451d0c72378dc70fff7ee0caa5395fa66b459f
id8436 <id8436@naver.com>
2024-10-29 16:10:24 +0900

    동료평가 설문 열람 못하게 수정.

--- school_report/admin.py
+++ school_report/admin.py
@@ -17,7 +17,10 @@ admin.site.register(HomeworkQuestion)
 class HomeworkSubmitAdmin(admin.ModelAdmin):
     search_fields = ['target_profile__name']  # 제목과 저자 이름으로 검색 가능
 admin.site.register(HomeworkSubmit, HomeworkSubmitAdmin)
-admin.site.register(HomeworkAnswer)
+#admin.site.register(HomeworkAnswer)
+class HomeworkAnswerAdmin(admin.ModelAdmin):
+    search_fields = ['target_profile__name']  # 제목과 저자 이름으로 검색 가능
+admin.site.register(HomeworkAnswer, HomeworkAnswerAdmin)
 admin.site.register(HomeworkBox)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'admin')
