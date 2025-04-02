3d427f15868c33d6d73eca3d202969608cc94276
id8436 <id8436@naver.com>
2025-04-02 12:22:10 +0900

    https 설정

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.service_settings')
 
 application = get_wsgi_application()
+
+# https 설정용
+os.environ['HTTPS'] = 'on'
